38cbd22847ed5e8a0b615c8fe91eb9d0265319a3
JackingChen <JackingChen@users.noreply.github.com>
2015-11-30 22:08:20 +0800

    Update ASAS_lab6_LinPred_2015.m

--- lab6/ASAS_lab6_LinPred_2015.m
+++ lab6/ASAS_lab6_LinPred_2015.m
@@ -5,7 +5,10 @@
 clear; close all;
 
 DIR = './';
-FILENAME = 'a.wav';
+filenames=dir('./*.wav');
+formant_peaks=cell(5,1);
+for fn=1:length(filenames)
+FILENAME = filenames(fn).name
 
 %FILENAME = 'EE6641-hw4-i.wav';
 [y,fs1] = audioread([DIR FILENAME]);
@@ -31,6 +34,8 @@ numFrames = floor(length(y)/L);
 excitat = zeros(size(y));
 e_n = zeros(p+L,1);
 
+formant_peaks{fn}=zeros(numFrames,2);
+
 LPcoeffs = zeros(p+1,numFrames);
 Kcoeffs = zeros(p,numFrames); % reflection coeffs
 
@@ -45,10 +50,11 @@ else
 end
 
 %% Linear prediction and estimation of the source e_n
-win = ones(L,1); % Rectangular window.
+% win = ones(L,1); % Rectangular window.
+win=hamming(L);
 for kk = 1:numFrames
     ind = (kk-1)*L+1:kk*L;
-    ywin = y_emph(ind).*win';
+    ywin = y_emph(ind).*win;
     %A = lpc(ywin,p); %% This is actually the more direct way to obtain the
     % LP coefficients. But, in this script, we used levinson() instead
     % because it gives us the "reflection coefficients". 
@@ -66,13 +72,15 @@ for kk = 1:numFrames
         e_n = filter(A,[1],ywin_extended);
     end
     excitat(ind) = e_n(p+1:end);
-    figure(1);
+%     figure(state);
     subplot(311);
     plot(ind/fs*1000, y(ind));
+    title('y(ind)');
     xlabel('ms')
     set(gca,'xlim',[kk-1 kk]*framelen*1000);
     subplot(312);
     plot(ind/fs*1000, e_n(p+1:end));
+    title('e_n(p+1:end)');
     set(gca,'xlim',[kk-1 kk]*framelen*1000);
    
     subplot(313);
@@ -83,13 +91,47 @@ for kk = 1:numFrames
     offset = max(Hmag) - max(Ymag);
     plot(ff,Hmag); hold on;
     plot(ff,Ymag+offset,'r'); hold off;
+    title('Hmag(blue) Ymag+offset(red)');
     set(gca,'xlim',[0 fs/2],'ylim',[Hmax-50, Hmax+5]);
     xlabel('Hz')
     drawnow;
     %pause;
     LPcoeffs(:,kk) = A;
     Kcoeffs(:,kk) = K;
+    
+    [Peak I]=findpeak (abs(Hmag),'MinPeakDistance',100);
+    [Peak_sort II]=sort(Peak,'descend');
+    first_freq=I(1);
+    secnd_freq=I(2);
+    diff_f1f2=secnd_freq-first_freq;
+    fn
+    formant_peaks{fn}(kk,1)=first_freq;
+    formant_peaks{fn}(kk,2)=secnd_freq;
+    
 end
+end
+figure(10)
+niters = 5;
+colors={'ro', 'go', 'bo', 'ko', 'co'};
+% for fn=1:length(filenames)
+% 
+% plot(formant_peaks{fn}(:,1),formant_peaks{fn}(:,2),colors{fn});
+% legend(filenames(fn).name);
+% % set(gca, 'color',[0 0 1]);
+% hold on;
+% end
+% hold off
+% figure
+plot(formant_peaks{1}(:,1),formant_peaks{1}(:,2),'ro',...
+    formant_peaks{2}(:,1),formant_peaks{2}(:,2),'g+',...
+    formant_peaks{3}(:,1),formant_peaks{3}(:,2),'b*',...
+    formant_peaks{4}(:,1),formant_peaks{4}(:,2),'kx',...
+    formant_peaks{5}(:,1),formant_peaks{5}(:,2),'c.'...
+);
+legend(filenames(1).name,filenames(2).name,filenames(3).name,filenames(4).name,filenames(5).name);
+xlabel('First formant frequency F1 (hz)');
+ylabel('Second formant frequency F2 (hz)');
+
 % play the estimated source signal
 soundsc(excitat,fs); 
 % show how cosistent the K-coefficients are.
